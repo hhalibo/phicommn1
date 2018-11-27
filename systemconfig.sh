ce052d8cd03b6da5c68afbc7cd6492ed28a9b765
hhalibo <gao13903408669@gmail.com>
2018-11-27 17:31:12 +0800

    Create systemconfig.sh
    First root system configuration

---
+++ systemconfig.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo
+echo "MUST BE ROOT，Running System Configurations"
+echo
+
+echo "stop serial-getty@ttyS0.service"
+sudo systemctl stop serial-getty@ttyS0.service;
+sudo systemctl daemon-reload;
+sudo systemctl disable serial-getty@ttyS0.service;
+
+echo "Run apt-update"
+sudo cp /boot/dtb/BCM4345C0.hcd  /lib/firmware/brcm;
+sudo apt update; 
+sudo apt upgrade -y;
+sudo apt install sudo net-tools -y;
+sudo apt autoremove;
+sudo nano /etc/sudoers
