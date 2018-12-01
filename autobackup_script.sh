ed0d4b4347f08854a915828486d32154ed9dc16c
hhalibo <gao13903408669@gmail.com>
2018-12-01 17:50:01 +0800

    Update autobackup_script.sh

--- autobackup_script.sh
+++ autobackup_script.sh
@@ -4,6 +4,11 @@ echo
 echo "Autobackup write script for Hassbian"
 echo
 
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run with sudo. Use \"sudo ${0} ${*}\"" 1>&2
+   exit 1
+fi
+
 echo "Write to scripts.yaml "
 sudo cat >> /home/homeassistant/.homeassistant/scripts.yaml <<'EOF'
 autobackup:
