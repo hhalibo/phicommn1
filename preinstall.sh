02863174fd65922dc07dd8faf44fc6e484a8f4ac
hhalibo <gao13903408669@gmail.com>
2018-12-03 16:05:31 +0800

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -4,10 +4,10 @@ echo
 echo "home-assistant install for Hassbian"
 echo
 
-if [ `whoami` != "pi" ];then
-    echo "!!! ONLY user=pi CAN RUN THIS SHELL SCRIPT!!!"
-    exit 0
-fi
+#if [ `whoami` != "pi" ];then
+#    echo "!!! ONLY user=pi CAN RUN THIS SHELL SCRIPT!!!"
+#    exit 0
+#fi
 
 echo "Running apt-get preparation"
 sudo apt update;
