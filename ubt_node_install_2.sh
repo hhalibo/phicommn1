278a6d67b3212cdaa916ef42caf4cc55f8b8b040
hhalibo <gao13903408669@gmail.com>
2019-04-26 18:13:55 +0800

    Create ubt_node_install_2.sh

---
+++ ubt_node_install_2.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo
+echo "node-red install script2 for Hassbian"
+echo
+
+sudo cat >> /etc/systemd/system/nodered.service <<'EOF'
+[Unit]
+Description=Node Red
+After=network.target
+
+[Service]
+User=root
+Group=root
+Type=simple
+ExecStart=/usr/bin/node-red -s /home/pi/.node-red/settings.js -u home/pi/.node-red
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Making Node-Red boot Autostart"
+sudo systemctl daemon-reload;
+sudo systemctl enable nodered.service;
+sudo systemctl start nodered.service;
+
+sleep 15
+
+echo "Installation done."
+sudo systemctl stop nodered.service;
+cd ~/.node-red
+
+echo "Installation of Nodered components."
+sudo npm install node-red-contrib-home-assistant-websocket;
+sudo npm install node-red-node-email;
+sudo npm install node-red-dashboard;
+sudo npm install node-red-contrib-modbustcp;
+sudo npm install node-red-contrib-pushbear;
+sudo npm install node-red-contrib-moment;
+sudo npm install node-red-contrib-simpletime;
+
+echo "All Done.Restart Nodered!PLEASE CTRL+C!PLEASE CTRL+C!PLEASE CTRL+C!PLEASE CTRL+C!"
+sudo systemctl restart nodered.service
+
+echo
+echo "Installation done."
+echo
