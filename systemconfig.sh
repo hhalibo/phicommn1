bd7307e9c7b82f30bb7010eafc09f4c8e08755ed
hhalibo <gao13903408669@gmail.com>
2019-04-26 15:41:57 +0800

    Update systemconfig.sh

--- systemconfig.sh
+++ systemconfig.sh
@@ -14,6 +14,7 @@ sudo cp /boot/dtb/BCM4345C0.hcd  /lib/firmware/brcm;
 sudo apt update; 
 sudo apt upgrade -y;
 sudo apt install sudo net-tools zip default-libmysqlclient-dev network-manager -y;
+sudo systemctl start NetworkManager.service;
 sudo apt install bluetooth avahi-daemon python3-venv  python3-pip python3-dev libbluetooth-dev build-essential libssl-dev libffi-dev  python-dev libudev-dev -y;
 sudo apt autoremove;
 sudo nano /etc/sudoers;
