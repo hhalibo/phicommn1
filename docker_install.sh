88a2f7512af0f2cd7cef4a298a4dcb37cccba2f2
hhalibo <gao13903408669@gmail.com>
2019-12-01 16:57:52 +0800

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -14,7 +14,10 @@ echo "add the GPG key for the official Docker repository to your system"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 echo "Add the Docker repository to APT sources"
-sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository \
+   "deb [arch=arm64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
 echo "Redo apt update"
 sudo apt update
